c1410c562a26c976f393242a4aaac2c1cd4e166b
Chakradhar Reddy Vonteddu <chakradhar@dhcp-206-87-99-81.ubcsecure.wireless.ubc.ca>
2026-03-22 20:07:46 -0700

    Data wrangling to merge datasets and add missing values

--- scripts/01_wrangling.R
+++ scripts/01_wrangling.R
@@ -1,21 +1,52 @@
+library(tidyr)
+library(dplyr)
+
+#Adds missing dates in sequence and then applies a forward fill to add data for holidays/weekends
+fill_missing_values <- function(df, date_col = "observation_date") {
+  
+  all_dates <- data.frame(observation_date = seq(min(df[[date_col]]), max(df[[date_col]]), by = "day"))
+  
+  df <- df %>%
+    full_join(all_dates, by = "observation_date")
+  df <- fill(df, -observation_date, .direction = "down")
+  
+  return(df)
+}
+
 # load raw data
 indpro <- read.csv("data/raw/INDPRO.csv")
 yield_curve <- read.csv("data/raw/T10Y2Y.csv")
 cpi <- read.csv("data/raw/CPI.csv", skip = 11)
 vix <- read.csv("data/raw/VIXCLS.csv")
-sp500 <- read.csv("data/raw/S&P 500.csv")
-
+sp500 <- rename(read.csv("data/raw/S&P 500.csv"), observation_date = Date, sp500 = Price)[,c("observation_date","sp500")]
 
 # change data type of observation_date 
 indpro$observation_date <- as.Date(indpro$observation_date)
 yield_curve$observation_date <- as.Date(yield_curve$observation_date)
 vix$observation_date <- as.Date(vix$observation_date)
-sp500$Date <- as.Date(sp500$Date)
+sp500$observation_date <- as.Date(sp500$observation_date)
 
-
+#Apply fill_missing_values function to daily datasets only
+vix <- fill_missing_values(vix)
+yield_curve <- fill_missing_values(yield_curve)
 
 tail(cpi)
 tail(indpro)
 tail(yield_curve)
 tail(vix)
 tail(sp500)
+
+#Transform CPI data-set (format needs to be changed to match other data-sets)
+cpi <- pivot_longer(cpi, cols = Jan:Dec, names_to = "month", values_to = "CPI") #change from wide to long format
+cpi$observation_date <- paste0(cpi$Year,"-",cpi$month, "-01") #assign default date to be the first of every month
+cpi <- cpi[,c("observation_date","CPI")]
+cpi$observation_date = as.Date(cpi$observation_date, format = "%Y-%b-%d")
+
+#Join multiple dataframes
+merged_data <- cpi %>%
+  inner_join(vix, by = "observation_date") %>%
+  inner_join(yield_curve, by = "observation_date") %>%
+  inner_join(indpro, by = "observation_date") %>%
+  inner_join(sp500, by = "observation_date")
+
+
