b3ad4267cb4861800920ce416dac26702841036d
Chakradhar Reddy Vonteddu <chakradhar@dhcp-206-87-99-81.ubcsecure.wireless.ubc.ca>
2026-03-28 15:57:30 -0700

    Added code for exponential smoothing methods and forecasts

--- scripts/03_fit-models.R
+++ scripts/03_fit-models.R
@@ -37,8 +37,49 @@ iid_fc <- function(train, holdout, iprint = FALSE) {
   return(list(rmse = sqrt(sse / n_holdout), fc = fc_vec))
 }
 
+esm_fc <- function(train, holdout, alpha, level, iprint = FALSE) {
+  nh <- length(holdout)
+  sse <- 0
+  fc_vec <- numeric(nh)
+  L <- level 
+  for (i in 1:nh) {
+    fc <- L
+    fc_vec[i] <- fc
+    yt <- holdout[i]
+    fcerror <- yt - fc
+    sse <- sse + (fcerror^2)
+    L <- alpha * yt + (1 - alpha) * L
+  }
+  return(list(rmse = sqrt(sse/nh), fc = fc_vec))
+}
+
+lholt_fc <- function(train, holdout, alpha, beta, level, slope, iprint = FALSE) {
+  nh <- length(holdout)
+  sse <- 0
+  fc_vec <- numeric(nh)
+  L <- level
+  b <- slope
+  for (i in 1:nh) {
+    fc <- L + b
+    fc_vec[i] <- fc
+    yt <- holdout[i]
+    fcerror <- yt - fc
+    sse <- sse + (fcerror^2)
+    L_old <- L
+    L <- alpha * yt + (1 - alpha) * (L_old + b)
+    b <- beta * (L - L_old) + (1 - beta) * b
+  }
+  return(list(rmse = sqrt(sse / nh), fc = fc_vec))
+}
+
 persist_results = persist_fc(train, holdout)
 iid_results = iid_fc(train, holdout)
+
+esm_model = HoltWinters(train, beta = F, gamma = F)
+esm_results = esm_fc(train, holdout, esm_model$alpha, esm_model$coefficients[1])
+
+lholt_model = HoltWinters(train, gamma = F)
+lholt_results = lholt_fc(train, holdout, lholt_model$alpha, lholt_model$beta, lholt_model$coefficients[1], lholt_model$coefficients[2])
 
 auto.arima(train,stationary = T, seasonal = F, stepwise = F)
 
