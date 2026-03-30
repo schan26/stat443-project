a0403c6dfb30c0c9bf6c8aa74b61fd8afcf61ccf
Tai <taitaitaitt123@gmail.com>
2026-03-30 00:47:01 -0700

    ACF and PACF fix

--- scripts/02_EDA.R
+++ scripts/02_EDA.R
@@ -2,8 +2,6 @@ library(forecast)
 
 data <- read.csv("data/processed/cleanedData.csv")
 
-train = window(data_ts, start = c(1990,2), end = c(2015,4)) #train data from 1990-02 to 2015-04
-holdout = window(data_ts, start = c(2015,5)) #holdout data from 2015-05 onwards
 data$observation_date <- as.Date(data$observation_date)
 
 vars <- c("CPI", "VIXCLS", "T10Y2Y", "INDPRO", "sp500_ret", "Gold", "Silver")
