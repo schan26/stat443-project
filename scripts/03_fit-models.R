a56199591a7548e1399fe9732d3c15ad469c7b03
Chakradhar Reddy Vonteddu <chakradhar@dhcp-206-87-102-154.ubcsecure.wireless.ubc.ca>
2026-04-03 19:53:28 -0700

    Formatting changes, added RMSE and FC table for all forecasting methods

--- scripts/03_fit-models.R
+++ scripts/03_fit-models.R
@@ -92,6 +92,7 @@ data <- read.csv("data/processed/cleanedData.csv")
 data_ts <- ts(data$sp500_ret, start = data_start, frequency = 12)
 
 ntrain = ceiling(nrow(data)*0.7)
+nholdout = nrow(data) - ntrain
 train_end = c(2015,4)
 holdout_start = c(2015,5)
 
@@ -128,6 +129,7 @@ arma_alt3_results = arima_fc(data_ts,length(train),c(0,0,2),c(0,0,0),"ML",alt_ar
 arma_alt4_results = arima_fc(data_ts,length(train),c(1,0,0),c(0,0,0),"ML",alt_arma4$coef,include.mean = T)
 arma_alt5_results = arima_fc(data_ts,length(train),c(2,0,0),c(0,0,0),"ML",alt_arma5$coef,include.mean = T)
 
+
 #Model fitted by auto.arima, which is white noise, outperforms alternate models on both AIC and holdout-set RMSE.
 
 #ARMAX
@@ -137,10 +139,10 @@ BAA_ts = ts(diff(data$BAA10YM), start = data_start, frequency = 12)
 BAA_train = window(BAA_ts, start = data_start, end = train_end)
 BAA_holdout = window(BAA_ts, start = holdout_start)
 
-ccf(BAA_train,train)
+ccf(BAA_train,train, main = "CCF of differenced BAA spread & SP500 returns", ylab = "CCF")
 
-acf(BAA_train)
-pacf(BAA_train)
+acf(BAA_train, main = "ACF of differenced BAA spread")
+pacf(BAA_train, main = "PACF of differenced BAA spread")
 
 BAA_model = auto.arima(BAA_train, stationary = T, seasonal = F)
 BAA_results = arima_fc(BAA_ts,length(BAA_train),c(2,0,1),c(0,0,0),"ML",BAA_model$coef,include.mean = F)
@@ -155,10 +157,10 @@ vix_diff_ts = ts(vix_diff, start = data_start, frequency = 12)
 vix_train = window(vix_diff_ts, start = data_start, end = train_end)
 vix_holdout = window(vix_diff_ts, start = holdout_start)
 
-acf(vix_train)
-pacf(vix_train)
+acf(vix_train, main = "ACF of differenced VIX" )
+pacf(vix_train, main = "PACF of differenced VIX")
 
-ccf(vix_train,train)
+ccf(vix_train,train, main = "CCF of differenced VIX & SP500 returns")
 
 vix_model = auto.arima(vix_train, stationary = T, seasonal = F)
 vix_results = arima_fc(vix_diff_ts,length(vix_train),c(0,0,3),c(0,0,0),"ML",vix_model$coef,include.mean = F)
@@ -175,12 +177,25 @@ df = data.frame(vix_fc = vix_fc[1:(ntotal-1)],
 reg = lm(sp500_ret ~ vix_fc + BAA_fc, data = df[1:ntrain,])
 print(summary(reg))
 
-acf(reg$residuals)
-pacf(reg$residuals)
+acf(reg$residuals, main = "ACF of regression residuals")
+pacf(reg$residuals, main = "PACF of regression residuals")
 
 #Residuals look like white noise so no further model needs to be fitted for the residuals.
 
 pred_reg = predict(reg, df[(ntrain+1):nrow(df),])
 
 rmse_reg = sqrt(mean((df$sp500_ret[(ntrain+1):nrow(df)] - pred_reg)^2))
+
+#Initially, the focus was on finding leading predictors but most predictors had either a weak leading effect or no leading effect at all over the S&P 500, which led to a worser model. 
+#Hence, the focus shifted to finding variables that had a strong lag 0 correlation with the SP500 returns, and use the 1-step forecasts of these variables in the holdout set to build a good ARMAX model. Two such variables were VIX and Baa Corporate bond yield relative to yield on 10-Year Treasury Constant Maturity, which had moderate lag 0 correlations with the SP500 returns over the training set.
+
+rmse_table = cbind(iid_results$rmse, persist_results$rmse, esm_results$rmse, lholt_results$rmse, arma_results$rmse, rmse_reg)
+colnames(rmse_table) = c("iid_rmse","persist_rmse","esm_rmse","lholt_rmse","arma_rmse","armax_rmse")
+rownames(rmse_table) = NA
+
+fc_table = cbind(data$sp500_ret[(ntrain+1):(ntotal-1)],iid_results$fc[1:(nholdout-1)],persist_results$fc[1:(nholdout-1)],esm_results$fc[1:(nholdout-1)],lholt_results$fc[1:(nholdout-1)],arma_results$fc[1:(nholdout-1)],c(pred_reg))
+colnames(fc_table) = c("holdout","iid_fc","persist_fc","esm_fc","lholt_fc","arma_fc","armax_fc")
+rownames(fc_table) = data$observation_date[(ntrain+1):(ntotal-1)]
+
+
 
